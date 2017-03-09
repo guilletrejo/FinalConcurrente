8c9b2fd2029b893c727eb49dda4265d5e0a191dd
Matthew Aguerreberry <matt_ague@hootmail.com>
2017-03-09 11:21:15 -0300

    primera aproximacion a los hilos

--- src/pkt/PiezaC.java
+++ src/pkt/PiezaC.java
@@ -2,15 +2,29 @@ package pkt;
 
 public class PiezaC implements Runnable {
 
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	private int estado; 
+	private int[] transicion = {31,32,33,34,35,36};
+	private GestorMonitor GM;
 
+	//	constructor
+
+	public PiezaC (GestorMonitor GM){
+		this.GM = GM;
+		this.estado = 0; 
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public void run() {
+		for(int ii = 0; ii < transicion.length; ii++){			
+			while(!GM.disparar_transicion(transicion[ii]));
+		}
+	}
 
+	private int get_estado(){
+		return this.estado;
+	}
+
+	private int[] get_transicion(){
+		return this.transicion;
 	}
 
 }

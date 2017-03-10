141f8e9ea84cf37534a75fab44d5d4fb1e7d2ae8
nati <nati@DESKTOP-I4D5D90>
2017-03-09 23:25:03 -0300

    Agrego red de petri con logica que parece funcionar

--- src/pkt/Main.java
+++ src/pkt/Main.java
@@ -1,11 +1,34 @@
 package pkt;
 
 public class Main {
-
+	private static String[] transiciones = {"T0","T11","T12","T13","T15","T16","T17","T18","T19","T21","T22","T23","T24","T3","T31","T32","T33","T34","T35","T36"};
+	//private static String[] transiciones = {"T1","T2"};
+	private static String[] hab = {"False", "True" };
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		GestorMonitor GDM = new GestorMonitor();
-
+		//GestorMonitor GDM = new GestorMonitor();
+		RdP rdp = new RdP();
+		int [] sens = rdp.sensibilizadas();
+		 for (int j = 0; j < sens.length; j++) {
+			 System.out.println(j + "->" +transiciones[j] + " = " + hab[sens[j]] + " ");
+		 }
+		System.out.println();
+		System.out.println("[MAIN] DISPARO  " + transiciones[0]+ " = "  + rdp.disparar(0));
+		System.out.println();
+		sens = rdp.sensibilizadas();
+		 for (int j = 0; j < sens.length; j++) {
+		     System.out.println(j + "->" + transiciones[j] + " = " + hab[sens[j]] + " ");
+		     
+		 }
+		System.out.println();
+		System.out.println("[MAIN] DISPARO " + transiciones[2] +" = " + rdp.disparar(2));
+		System.out.println();
+		
+		sens = rdp.sensibilizadas();
+		 for (int j = 0; j < sens.length; j++) {
+		     System.out.println(j + "->" + transiciones[j] + " = " + hab[sens[j]] + " ");
+		     
+		 }
 	}
 
 }
--- src/pkt/RdP.java
+++ src/pkt/RdP.java
@@ -3,42 +3,14 @@ package pkt;
 public class RdP {
 	private static int n_p = 30;
 	private static int n_t = 20;
+	private static String[] plazas = {"InterbloqPzaB","m1","m2","m3","m4","P10","P11","P12","P13","P14","P15","P16","P17","P18","P20","P21","P22","P23","P27","P28","P30","P31","P32","P33","P34","P35","r1","r2","r3"};
+	//private static String[] plazas = {"P1","P2","P3","P4"};
 	
 	private int marcado_inicial[][] = {{1},{1},{1},{1},{1},{10},{0},{0},{0},{0},{0},{0},{0},
 										{0},{10},{0},{0},{0},{1},{1},{10},{0},{0},{0},{0},{0},
 										{1},{1},{1}};
 	
-	private int matriz_pre [][] = {	{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},//I-
-									{0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0},
-									{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0},
-									{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
-									{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
-									{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
-									{0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
-									{0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
-									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
-									{0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-									{0,0,0,0,1,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
-									{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0}};
-	
-	private int matriz_post [][] = {{0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0}, //I+
+	private int matriz_pre [][] = {{0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0}, //I-
 									{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 									{0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
 									{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
@@ -56,7 +28,7 @@ public class RdP {
 									{0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
 									{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
 									{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
-									{1,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+									{0,0,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
 									{0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
 									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
 									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
@@ -66,12 +38,46 @@ public class RdP {
 									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
 									{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
 									{0,0,0,1,0,0,0,0,0,1,0,1,0,1,0,0,1,0,0,0},
-									{0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0},
-									{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0},
-									};
+									{0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,0,0,0,0,0}};
 
+	
+	private int matriz_post [][] = {{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+									{0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0},
+									{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,1,0,0,0},
+									{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+									{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0},
+									{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,1},
+									{0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
+									{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0},
+									{0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
+									{0,0,0,0,1,1,0,0,0,0,1,0,1,0,0,0,0,1,0,0},
+									{0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0}};
+	
+
+	//private int matriz_pre [][] = {{1,0},{0,1},{0,2},{0,0}};
+	//private int matriz_post [][] = {{0,1},{1,0},{1,0},{0,1}};
+	//private int marcado_inicial[][] = {{1},{0},{2},{1}};
+	
 	private int matriz_i [][];// = matriz_post - matriz_pre;
-	private int vector_dt[][] = new int[1][n_t]; // vector de transiciones a disparar
+	// vector de transiciones a disparar
 	private int marcado_actual[][];
 	
 	
@@ -84,9 +90,9 @@ public class RdP {
 	
 	private static int[][] producto(int A[][], int B[][]){
 		int suma = 0;
-		int result[][] = new int[A.length][B.length];
+		int result[][] = new int[A.length][B[0].length];
 		for(int i = 0; i < A.length; i++){
-			for(int j = 0; j < B.length; j++){
+			for(int j = 0; j < B[0].length; j++){
 				suma = 0;
 				for(int k = 0; k < B.length; k++){
 					suma += A[i][k] * B[k][j];
@@ -98,10 +104,10 @@ public class RdP {
 	}
 	
 	private static int[][] resta(int A [][], int B[][]){
-		int [][] resultado = new int[n_p][n_t];
+		int [][] resultado = new int[A.length][A[0].length];
 		
-		for(int i = 0; i < n_p; i++){
-			for(int j = 0; j <n_t; j++){
+		for(int i = 0; i < A.length; i++){
+			for(int j = 0; j < A[0].length; j++){
 				resultado[i][j] = A[i][j] - B[i][j];
 			}
 		}
@@ -109,33 +115,88 @@ public class RdP {
 	}
 	
 	private static int[][] suma(int A [][], int B[][]){
-		int [][] resultado = new int[n_p][n_t];
+		int [][] resultado = new int[A.length][A[0].length];
 		
-		for(int i = 0; i < n_p; i++){
-			for(int j = 0; j <n_t; j++){
+		for(int i = 0; i < A.length - 1; i++){
+			for(int j = 0; j < A[0].length - 1; j++){
 				resultado[i][j] = A[i][j] + B[i][j];
 			}
 		}
 		return resultado;
 	}
 	
-	public boolean disparar(int index){
-		int [][] resultado_disparo = new int[1][n_p];
-		vector_dt [1][index] = 1;
-		resultado_disparo = suma(marcado_actual,producto(matriz_i,vector_dt));
+	private static int[] suma_vec(int A [], int B[]){
+		int [] resultado = new int[A.length];
+		//System.out.print(A);
+			for(int j = 0; j < A.length; j++){
+				resultado[j] = A[j] + B[j];
+			}
+		return resultado;
+	}
+
+	private int[][] transponer(int[][] A){
+		int [][] resultado = new int[A[0].length][A.length];
 		
-		for(int i = 0;i < n_p;i++){
-			if (resultado_disparo[1][i] < 0){
-				System.out.printf("NO SE PUEDE DISPARAR");
+		for (int x=0; x < A.length; x++) {
+			  for (int y=0; y < A[x].length; y++) {
+			    resultado[y][x] = A[x][y];
+			  }
+		}
+		
+		return resultado;
+	}
+	
+	public boolean disparar(int index){
+		int [] resultado_disparo = new int[n_p];
+		int vector_dt[][] = new int[1][n_t]; 
+		vector_dt [0][index] = 1;
+
+		resultado_disparo = suma_vec(transponer(marcado_actual)[0],transponer(producto(matriz_i,transponer(vector_dt)))[0]);
+		
+//		System.out.println("----------------MARCADO ANTES DISPARO----------------");
+//		for (int j = 0; j < resultado_disparo.length; j++) {
+//		     //marcado_actual[j][0]=resultado_disparo[j]; 
+//		     System.out.println(plazas[j] + "->" + marcado_actual[j][0] + " ");
+//		 }
+//		System.out.println("--------------------------------------------------------");
+		
+		for(int i = 0;i < resultado_disparo.length;i++){
+			if (resultado_disparo[i] < 0){
+				System.out.printf("[RdP] NO SE PUEDE DISPARAR");
 				return false;
 			}
 		}
-		
-		marcado_actual = resultado_disparo;
-		
+		System.out.println("---------------------MARCADO DESPUES DE DISPARO--------------------");
+		for (int j = 0; j < resultado_disparo.length; j++) {
+		     marcado_actual[j][0]=resultado_disparo[j]; 
+		     System.out.println(plazas[j] + "->" + resultado_disparo[j] + " ");
+		 }
+		System.out.println("--------------------------------------------------------------");
+		System.out.printf("[RdP]  DISPARO OK");
+		System.out.println();
 		return true;
 	}
 
+	public int[] sensibilizadas(){
+		int [] suma_temp = new int[n_p];
+		int [] resultado = new int[n_t];
+		int flag = 1;
+		
+		for(int i = 0 ; i < matriz_i[0].length ; i++){
+			
+			suma_temp = suma_vec(transponer(marcado_actual)[0],transponer(matriz_i)[i]);
+			for(int j = 0; j < suma_temp.length ; j++){
+				if (suma_temp[j] < 0){
+					flag = 0;
+					break;
+				}
+			}
+			resultado[i]= flag;
+			flag = 1;
+		}
+			
+		return resultado;
+	}
 	
 	
 
